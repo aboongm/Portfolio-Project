fc65889e209a3f580489f59dd0b8d85fe6b44a83
aboongm <aboongm@yahoo.com>
2022-03-07 18:13:11 +0530

    Add changes: portfolio variable becomes textPortfolio. provide link for portfolio, about and contact from menu overlay

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('#hamburger');
 const close = document.querySelector('#close');
 const navigation = document.querySelector('.nav-ul');
-const portfolio = document.querySelector('.nav-text-1');
+const textPortfolio = document.querySelector('.nav-text-1');
 const about = document.querySelector('.nav-text-2');
 const contact = document.querySelector('.nav-text-3');
 
@@ -16,4 +16,11 @@ hamburger.addEventListener('click', (e) => {
 close.addEventListener('click', (e) => {
   hamburger.classList.remove('hide');
   navigation.classList.remove('show');
+});
+
+textPortfolio.addEventListener('click', (e) => {
+  e.stopPropagation();
+
+  navigation.classList.remove('show');
+  hamburger.classList.remove('hide');
 });
