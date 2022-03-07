29859cd280cde84abf368eb2ed00cd6e98b60b94
aboongm <aboongm@yahoo.com>
2022-03-07 17:18:01 +0530

    Add eventlistener to hamburger variable

---
+++ index.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('#hamburger');
+const close = document.querySelector('#close');
+const navigation = document.querySelector('.nav-ul');
+const portfolio = document.querySelector('.nav-text-1');
+const about = document.querySelector('.nav-text-2');
+const contact = document.querySelector('.nav-text-3');
+
+hamburger.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  navigation.classList.toggle('show');
+  hamburger.classList.add('display');
+});
