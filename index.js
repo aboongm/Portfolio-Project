5d3c11863b5c374c2d3d7e735404b2bbf30ab54d
aboongm <aboongm@yahoo.com>
2022-03-09 13:15:06 +0530

    Add the hasValue and validateEmail functions

--- index.js
+++ index.js
@@ -253,6 +253,33 @@ const EMAIL_REQUIRED = 'Please enter your email';
 const EMAIL_INVALID = 'Please enter a correct email address format';
 const INPUT_LOWERCASE = 'Please enter a lowercase input';
 
+function validateEmail(input, requiredMsg, invalidMsg, invalidLowercase) {
+  // check if the value is not empty
+  if (!hasValue(input, requiredMsg)) {
+    return false;
+  }
+  // validate email format
+  const emailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+
+  console.log(input.value);
+  // check if the input are all in lowercase
+  if (input.value === input.value.toLowerCase()) {
+    console.log('true');
+    return true;
+  } else {
+    console.log('false');
+    return showError(input, invalidLowercase);
+  }
+
+  return true;
+}
+
 form.addEventListener('submit', function (event) {
   // stop form submission
   event.preventDefault();
